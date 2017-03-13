974fb804d7176ecd16f69ffc1e1d872ccb49a124
nehadalvi <ndalvi1@uncc.edu>
2017-03-13 19:20:40 -0400

    AddMovieFragment

--- app/src/main/java/com/mad/myfavoritemoviesfragment/AddMovieFragment.java
+++ app/src/main/java/com/mad/myfavoritemoviesfragment/AddMovieFragment.java
@@ -1,12 +1,20 @@
 package com.mad.myfavoritemoviesfragment;
 
+import android.app.Activity;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.Serializable;
 
 
 /**
@@ -16,6 +24,10 @@ import android.view.ViewGroup;
  * to handle interaction events.
  */
 public class AddMovieFragment extends Fragment {
+
+    EditText et;
+    TextView tv;
+    Activity mainActivity;
 
     private OnFragmentInteractionListener mListener;
 
@@ -31,22 +43,24 @@ public class AddMovieFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_add_movie, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
+
+    public void onButtonPressed(Movie movie) {
         if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+            mListener.sendMovie(movie);
         }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
         }
+        mainActivity = (Activity)context;
     }
 
     @Override
@@ -55,18 +69,80 @@ public class AddMovieFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void sendMovie(Movie movie);
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        SeekBar sb = (SeekBar) getView().findViewById(R.id.seekBarRating);
+        tv = (TextView)getView().findViewById(R.id.textViewRatingOutput);
+        tv.setText(sb.getProgress()+"");
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                tv = (TextView) getView().findViewById(R.id.textViewRatingOutput);
+                tv.setText(progress+"");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+        });
+
+        getView().findViewById(R.id.buttonMovieAdd).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Movie movie= new Movie();
+
+                et = (EditText) getView().findViewById(R.id.editTextName);
+                movie.setName(et.getText().toString().trim());
+
+                et = (EditText) getView().findViewById(R.id.editTextDescription);
+                movie.setDescription(et.getText().toString().trim());
+
+                et = (EditText) getView().findViewById(R.id.editTextYear);
+                String temp= et.getText().toString().trim();
+                if(temp.equals("")||temp==null){
+                    movie.setYear(0);
+                }else{
+                    movie.setYear(Integer.parseInt(temp));
+
+                }
+
+                et = (EditText) getView().findViewById(R.id.editTextIMDBLink);
+                movie.setImdbLink(et.getText().toString().trim());
+
+                tv = (TextView) getView().findViewById(R.id.textViewRatingOutput);
+                temp= tv.getText().toString().trim();
+                movie.setRating(Integer.parseInt(temp));
+
+                Spinner mySpinner=(Spinner) getView().findViewById(R.id.spinnerGenre);
+                temp= mySpinner.getSelectedItem().toString();
+                movie.setGenre(temp);
+
+                if(movie.getName().equals("") || movie.getName().length()>50){
+                    Toast.makeText(mainActivity,getResources().getString(R.string.error_name),Toast.LENGTH_SHORT).show();
+                }else if(movie.getDescription().equals("") || movie.getDescription().length()>1000){
+                    Toast.makeText(mainActivity,getResources().getString(R.string.error_description),Toast.LENGTH_SHORT).show();
+                }else if(movie.getYear()<1800 || movie.getYear()>2030){
+                    Toast.makeText(mainActivity,getResources().getString(R.string.error_year),Toast.LENGTH_SHORT).show();
+                }else if(movie.getImdbLink().equals("")){
+                    Toast.makeText(mainActivity,getResources().getString(R.string.error_imdblink),Toast.LENGTH_SHORT).show();
+                }else{
+                    onButtonPressed(movie);
+                }
+            }
+        });
+
     }
 }
